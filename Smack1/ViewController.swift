37a4f241cc318c976a4ad9a3d426b24e1b47fad5
Jonah Travis <master@Jonahs-iMac.local>
2018-07-22 18:11:52 -0400

    Starting project

---
+++ Smack1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack1
+//
+//  Created by Jonah Travis on 7/22/18.
+//  Copyright © 2018 Jonah Travis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
